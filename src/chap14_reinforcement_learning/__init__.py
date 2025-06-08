4fc62a1d51f4a56284995f2bc120998120ca95e5
Reasle <2411202066@qq.com>
2025-06-08 12:52:09 +0800

    Update __init__.py

--- src/chap14_reinforcement_learning/__init__.py
+++ src/chap14_reinforcement_learning/__init__.py
@@ -6,8 +6,8 @@ from gym.envs.registration import registry, register, make, spec
 register(
     id = 'Copy-v0',
     entry_point = 'gym.envs.algorithmic:CopyEnv',
-    max_episode_steps = 200,
-    reward_threshold = 25.0,
+    max_episode_steps = 200,# 每个episode的最大步数，超过则自动终止
+    reward_threshold = 25.0,# 达到此奖励阈值视为环境被解决
 )
 
 register(
